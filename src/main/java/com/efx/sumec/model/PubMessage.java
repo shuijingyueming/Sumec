29315b5505076980c515889fbb1c76ae0ea7f0f2
shuijingyueming <91513481+shuijingyueming@users.noreply.github.com>
2023-09-02 07:56:23 +0800

    Update PubMessage.java

--- src/main/java/com/efx/sumec/model/PubMessage.java
+++ src/main/java/com/efx/sumec/model/PubMessage.java
@@ -6,9 +6,6 @@ import java.util.Map;
 public class PubMessage {
     public static Map dlmap = new HashMap();
     public static String serverUrl = "https://xhh.nbefx.com";
-//    小程序
-    public static String appid = "wx7070e33478430053";
-    public static String secret = "b8619427a12602ad66e19dbfbfe049cf";
 
     public static String access_token = "";
     public static String refresh_token = "";
