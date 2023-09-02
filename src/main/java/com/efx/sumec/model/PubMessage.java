6db39a6e78283055343af4063f675970ad25420f
孙伟丰 <734399928@qq.com>
2023-09-02 08:52:07 +0800

    wxm

--- src/main/java/com/efx/sumec/model/PubMessage.java
+++ src/main/java/com/efx/sumec/model/PubMessage.java
@@ -5,28 +5,19 @@ import java.util.Map;
 
 public class PubMessage {
     public static Map dlmap = new HashMap();
-    public static String serverUrl = "https://xhh.nbefx.com";
-
-    public static String access_token = "";
-    public static String refresh_token = "";
-
-    public static String access_tokenxx = "";
 
 
-    public static String getAccess_token() {
-        return access_token;
+    public static String serverUrl = "http://pvmall.com.cn";
+
+
+    private static ylxxz xxz;
+
+    public static ylxxz getXxz() {
+        return xxz;
     }
 
-    public static void setAccess_token(String access_token) {
-        PubMessage.access_token = access_token;
-    }
-
-    public static String getRefresh_token() {
-        return refresh_token;
-    }
-
-    public static void setRefresh_token(String refresh_token) {
-        PubMessage.refresh_token = refresh_token;
+    public static void setXxz(ylxxz xxz) {
+        PubMessage.xxz = xxz;
     }
 
 
